b1331740893c07dc438617b15b322fcd1f685afa
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-02-21 07:01:19 +0000

    [finish]validate

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,10 +6,11 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    if book.save
+    if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(book.id)
     else
+      @books = Book.all
       render :'index'
     end
   end
